9e409bd1d0d702ed7ff1a505066958010067444a
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-06-29 10:25:17 +0900

    loginメソッドの作成

--- webDev/javascript/user.js
+++ webDev/javascript/user.js
@@ -1,8 +1,18 @@
 class User {
-    
+
     deleted = 0;
 
     constructor(name){
         this.username = name;
+    }
+
+    login() {
+        if(this.deleted = 0){
+            console.log(`${this.username}はログインに成功しました`);
+        } else{
+            console.log(`${this.username}はログインに失敗しました`);
+        }
+        
+    
     }
 }
