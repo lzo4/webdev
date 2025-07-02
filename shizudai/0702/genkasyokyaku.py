94097d37d19398aaec93608f0f96a49d9861cb0a
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-07-02 16:40:44 +0900

    修正1

--- shizudai/0702/genkasyokyaku.py
+++ shizudai/0702/genkasyokyaku.py
@@ -1,9 +1,10 @@
 # 減価償却してくれる関数
 def syokyaku():
-    taiyou_year = int(input('耐用年数なんぼじゃい'))
-    num = int(input('金額いくら？'))
 
     try:
+        taiyou_year = int(input('耐用年数なんぼじゃい'))
+        num = int(input('金額いくら？'))
+
         syokyaku_rate = 1 / taiyou_year
         syokyaku_num = num*syokyaku_rate
         print(f"減価償却費:{syokyaku_num}です")
@@ -14,4 +15,4 @@ def syokyaku():
         print(e)
         print('数字で入れろやテメェこの野郎…')
 
-syokyaku()  
+syokyaku()
