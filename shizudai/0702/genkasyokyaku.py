78c9dd58c4b5bdf5e2662698887a4b243885c09d
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-07-02 16:36:53 +0900

    減価償却 python

---
+++ shizudai/0702/genkasyokyaku.py
@@ -0,0 +1,17 @@
+# 減価償却してくれる関数
+def syokyaku():
+    taiyou_year = int(input('耐用年数なんぼじゃい'))
+    num = int(input('金額いくら？'))
+
+    try:
+        syokyaku_rate = 1 / taiyou_year
+        syokyaku_num = num*syokyaku_rate
+        print(f"減価償却費:{syokyaku_num}です")
+    except ZeroDivisionError as e:
+        print(e)
+        print('0じゃ計算できるわけねえだろうが…ぶっ飛ばすぞ…')
+    except ValueError as e:
+        print(e)
+        print('数字で入れろやテメェこの野郎…')
+
+syokyaku()  
