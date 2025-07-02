98b834eb7252c5155594db83a04bbb12bd4121ea
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-07-02 17:07:58 +0900

    3

--- shizudai/0702/line_number.py
+++ shizudai/0702/line_number.py
@@ -1,8 +1,8 @@
 input_path = './designers.txt'
 output_path = './designers_numbered.txt'
 
-with open(input_path,) as input, \
+with open(input_path) as input, \
      open(output_path, mode='w')as output:
-    for i, line in enumerate(input, start='1'):
+    for i, line in enumerate(input, start=1):
         numbered_line = f"{i}:{line}"
-        output_path.write(numbered_line)
+        output.write(numbered_line)
