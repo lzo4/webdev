308ff44961a205f1074fdac1b04156d5656cc03a
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-07-17 02:22:25 +0900

    アロー関数に

--- webDev/learning/js/delay_message.js
+++ webDev/learning/js/delay_message.js
@@ -1,6 +1,7 @@
-function delayMessageFactory(dispFn,time) {
-    return function(msg){
-        setTimeout(()=>dispFn(msg),ms);
-    };
-    }
+const delayMessageFactory = (dispFn,time)=> msg => setTimeout(()=>dispFn(msg),ms);
 
+const dialog = delayMessageFactory(alert,2000);
+dialog("hello");
+
+const log = delayMessageFactory(console.log,2000);
+log("hi");
