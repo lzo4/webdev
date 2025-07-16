3d2aea2351e0c639897591974a6da8ce69f4ec0c
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-07-17 01:34:05 +0900

    修正2

--- webDev/learning/js/calc_test.js
+++ webDev/learning/js/calc_test.js
@@ -1,5 +1,5 @@
 function CalcAndDisp(calcFn,dispFn,...numbers) {
-    let result = calcFn(numbers);
+    let result = calcFn(...numbers);
     dispFn(result);
 }
 
@@ -13,10 +13,11 @@ function add(...numbers) {
 
 function minus(...numbers) {
     let sum = numbers[0];
-    for (let i =0;i < numbers.length;i++){
+    for (let i =1;i < numbers.length;i++){
         sum -= numbers[i];
     }
     return sum;
 }
 
 CalcAndDisp(add,console.log,...[9,10,0,4,10]);
+CalcAndDisp(minus,alert,...[5,4,4]);
