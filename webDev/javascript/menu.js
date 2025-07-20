e38f9c1fb5751547886ec18823fbc93b4abc2497
{youma.lzok@gmail.com} <youma.lzok@gmail.com>
2025-07-20 17:05:36 +0900

    メニュー処理

--- webDev/javascript/menu.js
+++ webDev/javascript/menu.js
@@ -3,6 +3,10 @@ const menu = new Map([
     ["クッキー",800],
     ["マカロン",1200],
     ["パスタ",1500],
+    ["アイス",700],
 ]);
 
 menu.set("カレー",1700);
+console.log(menu.get("ケーキ"));
+console.log(menu.has("パスタ"));
+menu.delete("アイス");
